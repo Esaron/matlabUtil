f02cea1513090fcb62fddc53a25d7d1fa60006cc
jdn <esaron88@gmail.com>
2014-07-15 19:58:57 -0400

    Added a faster, simpler whitening methodand renamed original.

--- whiten.m
+++ whiten.m
@@ -1,9 +1,9 @@
-function[W] = whiten(X, epsilon)
+function[xx] = whiten(x)
 %------------------------------------------------------------------------
 %
 % whiten.m:
 %   Whitens the input vectors (makes the covariance matrix the identity
-%   matrix; IE - WW'=I
+%   matrix; IE - WW'=I).  Faster and simpler than whiten2.
 %
 % Inputs:
 %   X: Vectors to whiten as rows
@@ -12,22 +12,12 @@ function[W] = whiten(X, epsilon)
 % Outputs:
 %   W: The whitened vectors as rows
 %
-% Adapted from the work of Colorado Reed, colorado-reed@uiowa.edu
-%   Original code: http://www.mathworks.com/matlabcentral
-%       /fileexchange/34471-data-matrix-whitening/content/whiten.m
-%
 %------------------------------------------------------------------------
 
-if ~exist('epsilon','var')
-    epsilon = 0.0001;
-end
-
-mu = mean(X, 2); 
-X = bsxfun(@minus, X, mu);
-A = X'*X;
-[V,D] = svd(A);
-whMat = sqrt(size(X,1)-1)*V*sqrtm(inv(D + eye(size(D))*epsilon))*V';
-W = X*whMat;  
-%invMat = pinv(whMat);
+c=cov(x');
+sq=inv(sqrtm(c));
+mx=mean(x');
+xx=x-mx'*ones(1,size(x,2));
+xx=2*sq*xx;
 
 end
---
+++ whiten2.m
@@ -0,0 +1,33 @@
+function[W] = whiten2(X, epsilon)
+%------------------------------------------------------------------------
+%
+% whiten2.m:
+%   Whitens the input vectors (makes the covariance matrix the identity
+%   matrix; IE - WW'=I
+%
+% Inputs:
+%   X: Vectors to whiten as rows
+%   epsilon: Step size
+%
+% Outputs:
+%   W: The whitened vectors as rows
+%
+% Adapted from the work of Colorado Reed, colorado-reed@uiowa.edu
+%   Original code: http://www.mathworks.com/matlabcentral
+%       /fileexchange/34471-data-matrix-whitening/content/whiten.m
+%
+%------------------------------------------------------------------------
+
+if ~exist('epsilon','var')
+    epsilon = 0.0001;
+end
+
+mu = mean(X, 2); 
+X = bsxfun(@minus, X, mu);
+A = X'*X;
+[V,D] = svd(A);
+whMat = sqrt(size(X,1)-1)*V*sqrtm(inv(D + eye(size(D))*epsilon))*V';
+W = X*whMat;  
+%invMat = pinv(whMat);
+
+end
